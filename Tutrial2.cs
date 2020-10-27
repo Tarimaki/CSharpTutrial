c731f90570f13461ec5d180abdb9f792d50e39c2
Tarimaki <50066955+Tarimaki@users.noreply.github.com>
2020-10-27 19:46:17 +0900

    Create Tutrial2.cs

---
+++ Tutrial2.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Tutrial2
+{
+  public static void Main()
+  {
+    int i = 0;
+    //if文
+    if(i == 0)
+    {
+      //i が 0の場合実行される
+    }
+    else
+    {
+      //i が 0以外の場合実行される
+    }
+    
+    /*
+     * 最初にiに0を代入し、
+     * 繰り返すたびにiに1を足していく
+     * iが10未満の場合は実行する
+    for(i = 0; i < 10; i++)
+    {
+      //ここが繰り返し実行される
+    }
+    
+    //i が 0の場合はwhileの中身を繰り返し実行する
+    while(i == 0)
+    {
+      //ここが繰り返し実行される
+    }
+    
+    //ほかにもSwitch文やforeach文などがある
+  }
+}
